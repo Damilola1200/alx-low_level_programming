8f14f935c4cb70091683e351b569e80e4cb5b229
Damilola1200 <jameschloe690@gmail.com>
2022-09-14 03:28:21 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - find if input is pos, neg, 0r none/0
+ * @n: Int to print sign from.
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
