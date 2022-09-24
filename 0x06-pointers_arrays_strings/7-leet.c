5212ee1b776b5674940befa05670598ff1b1b772
Damilola1200 <jameschloe690@gmail.com>
2022-09-23 22:45:08 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,27 @@
-#include "
+#include "main.h"
+
+/**
+ * leet - check the code for holberton school students.
+ * @str: string.
+ *
+ * Return: string.
+ */
+char *leet(char *str)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[indx1])
+	{
+		for (indx2 = 0; indx2 <= 7; indx2++)
+		{
+			if (str[indx1] == leet[indx2] ||
+			str[indx1] - 32 == leet[indx2])
+				str[indx1] = indx2 + '0';
+		}
+
+		indx1++;
+	}
+
+	return (str);
+}
